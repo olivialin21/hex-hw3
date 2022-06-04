fe3ffebec07613e928caa4b8646d7f6e8503fbfe
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-06-05 03:56:58 +0800

    all rwd finish

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function(e){
   // hover“首頁”時，選單展開
-  e.preventdefault;
-  $('#nav-index').hover(function(event){
+  $('#nav-index').hover(function(e){
     $('.nav-sublist').stop().slideToggle();
   });
   // hover nav-sublist 時，選單展開
-  $('.nav-sublist').hover(function(event){
+  $('.nav-sublist').hover(function(){
     $('.nav-sublist').stop().slideToggle();
   });
   // 漢堡選單
@@ -13,4 +12,12 @@ $(document).ready(function(e){
 		$(this).toggleClass('open');
     $('.nav').toggleClass('show');
   });
+  $('.order-block i').click(function(){
+    $(this).toggleClass('fa-heart-o')
+    $(this).toggleClass('fa-heart')
+  })
+  $('#form-cancel').click(function(e){
+    // e.preventdefault();
+    $('#map-form').trigger("reset")
+  })
 })
