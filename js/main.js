d1581bd5c56b3f1ef0a5dbcbfb0923a06607bbfa
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-06-05 00:41:41 +0800

    order page finish

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function(e){
   // hover“首頁”時，選單展開
+  e.preventdefault;
   $('#nav-index').hover(function(event){
     $('.nav-sublist').stop().slideToggle();
   });
   // hover nav-sublist 時，選單展開
   $('.nav-sublist').hover(function(event){
     $('.nav-sublist').stop().slideToggle();
+  });
+  // 漢堡選單
+  $('.nav-ham').click(function(){
+		$(this).toggleClass('open');
+    $('.nav').toggleClass('show');
   });
 })
